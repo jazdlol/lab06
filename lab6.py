2eeea61aa6bd939f4cb9ad67405d01dc57b6ba49
jazdlol <127228391+jazdlol@users.noreply.github.com>
2023-03-07 12:30:19 -0500

    Update lab6.py
    yes

--- lab6.py
+++ lab6.py
@@ -1,1 +1,28 @@
-print(hello.world)
+def encode(pass_w):
+    new_pass = ''
+    for i in pass_w:
+        if i == str(7):
+            digit = int(0)
+            new_pass += str(digit)
+        elif i == str(8):
+            digit = int(1)
+            new_pass += str(digit)
+        elif i == str(9):
+            digit = int(2)
+            new_pass += str(digit)
+        else:
+            digit = int(i) + 3
+            new_pass += str(digit)
+    return str(str(new_pass))
+
+
+if __name__ == '__main__':
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    selection = int(input('Please enter an option:'))
+    if selection == 1:
+        password = (input('Please enter your password to encode:'))
+        (encode(password))
